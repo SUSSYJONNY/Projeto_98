4d4174ea909048e1ee3b7fdbf69c440d93f95e7a
SUSSYJONNY <128854842+SUSSYJONNY@users.noreply.github.com>
2023-10-11 19:08:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ camera = document.getElementById("camera");
 Webcam.set({
     width: 500,
     height: 400,
-    image_format:  jpeg,
+    image_format:"jpeg",
     jpeg_quality: 90,
 })
 
@@ -64,13 +64,13 @@ console.log(img_id)
 
 Webcam.snap(function(data_uri){
 if(img_id=="selfie1"){
-document.getElementById("result1").innerHTML = '<img id="selfie1" src"'+data_uri'"/>';
+document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+data_uri+'"/>';
 }
 if(img_id=="selfie2"){
-    document.getElementById("result2").innerHTML = '<img id="selfie2" src"'+data_uri'"/>';
+    document.getElementById("result2").innerHTML = '<img id="selfie2" src="'+data_uri+'"/>';
     }
     if(img_id=="selfie3"){
-        document.getElementById("result3").innerHTML = '<img id="selfie3" src"'+data_uri'"/>';
+        document.getElementById("result3").innerHTML = '<img id="selfie3"= src"'+data_uri+'"/>';
         }
 })
 }
